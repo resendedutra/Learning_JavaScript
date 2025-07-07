434d85b7458491c1bfcf657c3027fc892b28fb53
Carlos Dutra <kduitba@gmail.com>
2025-07-07 18:51:08 -0300

    Commit Inicial

---
+++ string.js
@@ -0,0 +1,15 @@
+let nome = 'Carlos';
+let sobrenome = 'Dutra';
+let idade = 50;
+
+let mensagem1 = 'Meu nome é ' + nome + ' ' + sobrenome + ' e tenho ' + idade + ' anos de idade!';
+let mensagem2 = `Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos de idade!`;
+console.log('Meu nome é', nome, sobrenome, 'e tenho', idade, 'anos de idade!');
+console.log(mensagem1);
+console.log(mensagem2);
+
+const s = 'Olá Mundo!';
+console.log(s.length); // length é uma propriedade/atributo que conta o nº de caracteres
+console.log(s.toUpperCase()); //.toUpperCase é uma função/método deixa tudo maiúsculo
+console.log(s.toLowerCase()); //.toLowerCase é uma função/método deixa tudo minúsculo
+console.log(s.substring(4, 10)); //.substring é uma função/método captura parte de um texto
