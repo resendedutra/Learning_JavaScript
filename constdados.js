a4cef69e1abe2c8a105287e43d944a411398799b
resendedutra <81709927+resendedutra@users.noreply.github.com>
2025-07-07 22:06:16 +0000

    Arquivo adicionado ao projeto

---
+++ constdados.js
@@ -0,0 +1,20 @@
+/* Variáveis e tipos de dados*/
+const x = 10; // tipo de dado número inteiro
+const y = 2.5; // tipo de dado número decimal
+const nome = 'Carlos'; // tipo de dado string
+const sobrenome = 'Dutra'; // tipo de dado string
+const verdadeiro = true; // tipo de dado verdadeiro ou falso (boolean)
+const falso = false; // tipo de dado verdadeiro ou falso (boolean)
+const nulo = null; // tipo de dado nulo
+const nao_definido = undefined; // tipo de dado indefinido
+
+// Mostrar coisas no terminal
+console.log(nome, sobrenome);
+console.log(x);
+console.log(y);
+console.log(verdadeiro);
+console.log(falso);
+console.log(nulo);
+console.log(nao_definido);
+console.log(typeof(x)); // mostra o tipo do dado
+console.log(typeof(nome)); // mostra o tipo do dado
