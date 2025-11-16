99ccdecc00208926d4bc8aa0d85f8c73e5403d23
Carlos Dutra <kduitba@gmail.com>
2025-11-16 11:18:20 -0300

    Comentário de atualização

--- CURSO_CFB/imc.js
+++
@@ -1,9 +0,0 @@
-function calcular_imc(altura, peso){
-  let imc = peso / (altura * altura)
-  return imc
-}
-
-let altura = prompt("Digite sua altura em metros: ")
-let peso = prompt("Digite seu peso em kg: ")
-
-console.log (calcular_imc(altura, peso))
